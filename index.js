4efdb4dad197eb5fd521b5f3facc7cddc68cc4f8
Soo Jung Kim <soojung.kimm@gmail.com>
2024-01-05 10:13:26 -0800

    completed lab

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function increment(num){
+    return (num += 1);
+}
+
+function decrement(num){
+    return (num -= 1);
+}
+
+function makeInt(string){
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(string){
+    return parseFloat(string);
+}
